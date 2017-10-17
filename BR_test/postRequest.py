70df796060a537b686e4e3e338e0e1503f60816b
akenoq <akenoq@yandex.ru>
2017-10-17 03:47:26 +0300

    add course of moving

--- BR_test/postRequest.py
+++ BR_test/postRequest.py
@@ -1,18 +1,19 @@
-import json
 import sys
 import requests
+import random
+import json
 import time
 
-url = 'http://localhost:5555/datawriter'
-# url = 'https://brainrider.herokuapp.com/datawriter'
+# url = 'http://localhost:5555/datawriter'
+url = 'https://brainrider.herokuapp.com/datawriter'
 
 i = 0
 while True:
     i += 1
-
+    course = random.choice(["Forward", "Backward", "Left", "Right"])
     data = {
         "id": i,
-        "data": "hello_a"
+        "data": course
     }
 
     req = requests.post(url, json = data)
