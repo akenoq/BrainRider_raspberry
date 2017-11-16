64cb75887951abc77c94e9a31261747d9d6d832b
akenoq <akenoq@yandex.ru>
2017-11-16 06:15:42 -0800

    keyboard sending 300ms
    python server_keyboard.py

---
+++ Server/server_keyboard.py
@@ -0,0 +1,49 @@
+import asyncio
+
+from autobahn.asyncio.websocket import WebSocketServerProtocol, \
+    WebSocketServerFactory
+
+
+class MyServerProtocol(WebSocketServerProtocol):
+
+    def onConnect(self, request):
+        print("Client connecting: {0}".format(request.peer))
+
+    async def onOpen(self):
+        print("WebSocket connection open.")
+        isBinary = True
+        # start sending messages every second ..
+        while True:
+            # read from keyboard (_W, _A, _S, _D, _WA)
+            data = input(">>")
+            self.sendMessage(data.encode('utf8'))
+            # self.sendMessage(b"\x00\x01\x03\x04", isBinary=True)
+            await asyncio.sleep(0.3)
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            print("Binary message received: {0} bytes".format(len(payload)))
+        else:
+            print("Text message received: {0}".format(payload.decode('utf8')))
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
+    factory.protocol = MyServerProtocol
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, '0.0.0.0', 9000)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
